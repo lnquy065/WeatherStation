9fae00857f134bba6b2f31f91a3df97c6df4a489
LN Quy <lnquy065@gmail.com>
2018-08-01 02:20:22 +0700

    release v1.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <SPI.h>
 #include <LoRa.h>
-#include <SSD1306.h>
 #include <Arduino.h>
 
 #include "lib/SharpGP2Y10.h"
@@ -9,17 +8,20 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <MQ135.h>
+#include <SSD1306.h>
 
-
+String NODE_ID = "N01";
 
 // DEEP SLEEP MODE SETTING
 #define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP  3000        /* Time ESP32 will go to sleep (in seconds) */
+#define TIME_TO_SLEEP  5        /* Time ESP32 will go to sleep (in seconds) */
 
 RTC_DATA_ATTR int bootCount = 0;
 
-// OLED SETTING
+// OLED
 SSD1306 display(0x3c, 4, 15);
+byte OLED_FONT_SIZE =  12;
+byte OLED_LINE_OFFSET = 2;
 
 // LORA SETTING
 #define SS 18
@@ -33,6 +35,7 @@ const char* password = "0937437982";
 
 // SENSOR SETTING
 int counter = 0;
+#define BOOT_LED 12
 #define SENSOR_PIN_DUST_VO A0
 #define SENSOR_PIN_DUST_LEDPIN 2
 
@@ -40,9 +43,14 @@ int counter = 0;
 #define SENSOR_PIN_UV_VO A3
 #define SENSOR_PIN_CO2 A2
 
+#define BOOT_FAN 22
+
 SharpGP2Y10 sensorDust(SENSOR_PIN_DUST_VO, SENSOR_PIN_DUST_LEDPIN);
 MQ135 sensorCo2(SENSOR_PIN_CO2);
 ML8511 sensorUV(SENSOR_PIN_UV_VO, SENSOR_PIN_UV_VREF);
+
+String co2String, uvString, dustString;
+int lora_counter;
 
 // FUNCTION DECLARE
 void loraSend(String);
@@ -57,38 +65,68 @@ void initWiFi();
 void initSensors();
 void sendHTTP();
 
+void oledPrint(int, int, String);
 void print_wakeup_reason();
+void run();
 
 //============= BODY =======================
 void setup() {
   initSerial();
+  initPinMode();
   initOLED();
-  // initPinMode();
-  // initWiFi();
-  // // digitalWrite(25, HIGH);
-  // sendHTTP();
+  initLoRa();
+  delay(1000);
+  digitalWrite(BOOT_LED, HIGH);
   sensorCo2.calibrate();
+  Serial.println(bootCount);
+  //on fan
+   digitalWrite(BOOT_FAN, HIGH);
+  delay(3000);
+  run();
+  delay(1000);
+  
+
+  //go to sleep
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  esp_deep_sleep_start();
 }
 
 void loop() {
-  Serial.println("C02: " + String(sensorCo2.readCO2()) );
-  Serial.println("UV: "+ String(sensorUV.readUVIntensity()) );
-  Serial.println("Dust: "+ String(sensorDust.getDustDensity()) );
-  // float dustVal = sensorDust.getDustDensity();
-  // display.clear();
-  // display.setFont(ArialMT_Plain_10);
-  // display.drawString(3, 5, "Dust density: ");
-  // display.drawString(70, 5, String(dustVal));
-  // display.drawString(3, 15, "Co2 density: ");
-  // display.drawString(70, 15, String(dustVal));
-  // display.display();
 
-  delay(2000);
+}
+
+void run() {
+  bootCount+=1;
+  co2String = String(sensorCo2.readCO2()) ;
+  dustString = String(sensorDust.getDustDensity());
+  uvString = String(sensorUV.readUVIntensity());
+
+  loraSend( NODE_ID + "/" + co2String + "/" + dustString + "/" + uvString);
+
+  // Serial.println("C02: " + co2String);
+  // Serial.println("UV: " +uvString);
+  // Serial.println("Dust: " +dustString);
+  // Serial.println("Boot: " +bootCount);
+
+  // display.clear();
+  // display.setTextAlignment(TEXT_ALIGN_LEFT);
+  
+  // display.setFont(Arimo_Regular_12);
+  // display.drawString(0, 0, "Co2: " + co2String + " ppm");
+  // display.drawString(0, 12 + 3, "UV: " + uvString + " mW/cm²");
+  // display.drawString(0, 25 + 3, "Dust: " + dustString + " mg/m³");
+  // display.drawString(0, 39 + 3 , "LoRa sent: " + String(bootCount) + " pkgs");
+  // display.display();
 }
 
 
-
 //============== FUNCTION BODY
+
+void oledPrint(int x, int y, String s) {
+  char __s[sizeof(s+1)];
+  s.toCharArray(__s, sizeof(__s));
+  // oled.drawStr(0,1, __s );
+}
 
 void sendHTTP() {
    HTTPClient http;   
@@ -99,8 +137,7 @@ void sendHTTP() {
 
 void loraSend(String data) {
  LoRa.beginPacket() ;
-  LoRa.print("Hello ") ;
-  LoRa.print(counter) ;
+  LoRa.print(data);
   LoRa.endPacket();
 }
 
@@ -114,30 +151,34 @@ void initSerial() {
 void initPinMode() {
   pinMode(25, OUTPUT); //Onboard LED
   pinMode(16, OUTPUT);
+  pinMode(BOOT_LED, OUTPUT);
+  pinMode(BOOT_FAN, OUTPUT);
 }
 
 void initOLED() {
-  digitalWrite(16, LOW); //Reset OLED
+  digitalWrite(16, LOW);
   delay(50);
+   digitalWrite(16, HIGH);
   display.init();
-  display.display();
+  display.flipScreenVertically();  
 }
 
 void initLoRa() {
-  SPI.begin(5, 19, 27, 18);
-  LoRa.setPins(SS, RST, DI0);
-  LoRa.setSpreadingFactor(12);
-  LoRa.setCodingRate4(4/5);
-  LoRa.setSignalBandwidth(123E3);
-  LoRa.setPreambleLength(5);
-  delay(1000);
-  Serial.println("LoRa Initial");
-  if (!LoRa.begin(BAND)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
-  }
-  Serial.println("LoRa Initial OK!");
-  delay(2000);
+    SPI.begin(5, 19, 27, 18);
+    LoRa.setPins(SS, RST, DI0);
+    LoRa.setSpreadingFactor(12);
+    LoRa.setCodingRate4(4/5);
+    LoRa.setSignalBandwidth(123E3);
+    LoRa.setPreambleLength(5);
+    delay(1000);
+    Serial.println("LoRa Initial");
+    if (!LoRa.begin(BAND)) {
+      Serial.println("Starting LoRa failed!");
+      while (1);
+    }
+    Serial.println("LoRa Initial OK!");
+    delay(2000);
+  
 }
 
 void initWiFi() {
@@ -154,19 +195,3 @@ WiFi.begin(ssid, password);
   Serial.println(WiFi.localIP());
 }
 
-
-void print_wakeup_reason(){
-  esp_sleep_wakeup_cause_t wakeup_reason;
-
-  wakeup_reason = esp_sleep_get_wakeup_cause();
-
-  switch(wakeup_reason)
-  {
-    case 1  : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
-    case 2  : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
-    case 3  : Serial.println("Wakeup caused by timer"); break;
-    case 4  : Serial.println("Wakeup caused by touchpad"); break;
-    case 5  : Serial.println("Wakeup caused by ULP program"); break;
-    default : Serial.println("Wakeup was not caused by deep sleep"); break;
-  }
-}
